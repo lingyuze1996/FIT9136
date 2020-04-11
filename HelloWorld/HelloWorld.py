bb91f47d29495fc5a29b0790851392a06a93328d
lingyuze1996 <62694056+lingyuze1996@users.noreply.github.com>
2020-04-11 21:51:52 +1000

    HelloWorld 1.0

---
+++ HelloWorld/HelloWorld.py
@@ -0,0 +1,18 @@
+print("Hello, world!")
+i = [1, 90]
+
+summ = 0
+for j in i:
+    if j % 2 == 0:
+        summ += j
+print(summ)
+
+
+def eg(ii, jj):
+    k = 2 * ii + jj
+    return k ** 3
+
+
+print(eg(1, 3))
+
+
