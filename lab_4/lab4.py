4267e3a42d8fd0b1eb2e7e8d9fd353abb417c8c5
lingyuze1996 <62694056+lingyuze1996@users.noreply.github.com>
2020-04-12 18:08:27 +1000

    Update lab4.py

--- lab_4/lab4.py
+++ lab_4/lab4.py
@@ -1,4 +1,39 @@
-proceed = True
-while proceed:
-    print(proceed)
-    proceed = not proceed
+"""
+Lab 4: Data Types
+"""
+
+# Task 1: Sets
+
+s1 = {1, 2, 3} | {2, 3, 4}
+s2 = {1, 2, 3} & set()
+
+# Task 2: Lists vs Tuples
+
+
+# Task 3: Copies
+
+base = [[]] * 5
+base[1].append(1)
+
+# Task 4: Dictionary Keys
+
+
+
+# Task 5: Dictionary vs Lists
+
+
+
+# Task 6: Load of Strings
+
+strings = list()
+while True:
+    stringInput = input("Please enter a String: ")
+    if stringInput.lower() != "quit":
+        strings.append(stringInput)
+    else:
+        break
+print(",".join(strings))
+
+# Task 7: Analysing Literature
+
+# Task 8 Tic-Tac-Toe
